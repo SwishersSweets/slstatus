161312436cd6d664dd37fb477b140a869f3f59b3
SwishersSweets <111619350+SwishersSweets@users.noreply.github.com>
2023-10-24 11:04:36 -0400

    Update config.def.h

--- config.def.h
+++ config.def.h
@@ -67,9 +67,8 @@ static const char unknown_str[] = "n/a";
 */ 
 static const struct arg args[] = {
 	/* function format          argument */
-	{netspeed_tx,	 "UP %sB/s ", "eth0"		},
-	{netspeed_rx,	 "DOWN %sB/s ", "eth0"		},
-	{ipv4,		 "LAN %s ", "eth0"		},
+	{netspeed_tx,	 "%sB/s", "eth0"		},
+	{netspeed_rx,	 "DOWN", "eth0"			},
 	{cpu_perc,	 "CPU %s%% ", NULL 		},
 	{ram_used,	 "RAM %s ", NULL		},
 	{datetime,       "%s",	"%a %b %r"		},
