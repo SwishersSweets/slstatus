5b614b28c7480f19b6095bacf5c1a6db209d3091
SwishersSweets <111619350+SwishersSweets@users.noreply.github.com>
2024-01-16 10:36:14 -0500

    Add files via upload

--- config.def.h
+++ config.def.h
@@ -67,8 +67,8 @@ static const char unknown_str[] = "n/a";
 */ 
 static const struct arg args[] = {
 	/* function format          argument */
-	{netspeed_tx,	 "%s/", "eth0"		},
-	{netspeed_rx,	 " %s ", "eth0"			},
+	{netspeed_tx,	 "%sB/s /", "enp3s0"		},
+	{netspeed_rx,	 " %sB/s ", "enp3s0"			},
 	{cpu_perc,	 "CPU %s%% ", NULL 		},
 	{ram_used,	 "RAM %s ", NULL		},
 	{datetime,       "%s",	"%a %b %r"		},
